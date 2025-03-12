d3829dd828e894bc5e7a544b5d8003983776cab7
cys070124 <cys070124@gmail.com>
2025-03-12 20:14:14 +0900

    Feat : Google OAuth2, JWT 발급, Entity 생성

---
+++ src/main/java/org/example/bumitori_server/BumitoriServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.bumitori_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumitoriServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BumitoriServerApplication.class, args);
+    }
+
+}
