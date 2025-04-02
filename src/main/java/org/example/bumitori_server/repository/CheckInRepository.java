9d6d213d91e56d72e3a4799d4b4e90f8d9e069be
JeongSuHwan23 <jeongsuhwan@jeongsuhwan-ui-MacBookAir.local>
2025-04-02 12:15:12 +0900

    perf : 입사 현황 조회 페이지 성능 개선

--- src/main/java/org/example/bumitori_server/repository/CheckInRepository.java
+++ src/main/java/org/example/bumitori_server/repository/CheckInRepository.java
@@ -3,8 +3,12 @@ package org.example.bumitori_server.repository;
 import org.example.bumitori_server.entity.CheckIn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CheckInRepository extends JpaRepository<CheckIn, Long> {
-    Optional<CheckIn> findByUserId(Long userId);
+  Optional<CheckIn> findByUserId(Long userId);
+
+  List<CheckIn> findByUserIdIn(List<Long> userIds);
 }
+
